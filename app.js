64f4eb50cd56f4b5b6ed2e6eb0402dc44a3749a0
Christopher Tham <chriswt91@gmail.com>
2016-09-30 10:55:03 -0700

    Implement component rendering and props

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList items={['Cucumber', 'Kale']} />
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+  </ul>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
